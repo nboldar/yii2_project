118663dddd80b35991d29b9f298a6ccdcf68d745
nboldar <nboldar1@gmail.com>
2019-03-10 18:12:41 +0300

    public dir was renamed

--- config/test.php
+++ config/test.php
@@ -19,7 +19,7 @@ return [
             'useFileTransport' => true,
         ],
         'assetManager' => [
-            'basePath' => __DIR__ . '/../web/assets',
+            'basePath' => __DIR__ . '/../app/assets',
         ],
         'urlManager' => [
             'showScriptName' => true,
